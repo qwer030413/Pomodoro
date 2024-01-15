0c9ff0af3ba7dc60374c82f227e493a3f6174d79
Chris <comosjay@gmail.com>
2024-01-14 20:21:04 -0800

    minitab!

--- PomodoroProject/vite-project/src/components/Tabs/animatedTabs.tsx
+++ PomodoroProject/vite-project/src/components/Tabs/animatedTabs.tsx
@@ -1,13 +1,33 @@
 import { ReactElement } from 'react';
-import 'react-tabs/style/react-tabs.css';
-import { Tab, Tabs } from "@mui/material";
-
+import { useState } from 'react';
+import Timer from '../Timer/timer';
 export default function MiniTab(): ReactElement{
+    const [tabs, setTabs] = useState(1);
+    function updateTabs(id: number)
+    {
+        setTabs(id);
+    }
     return(
-        <div className='minicontainer'>
-            <div>Tab1</div>
-            <div>Tab1</div>
-            <div>Tab1</div>
+        <div className={'minicontainer' + tabs}>
+            <div className='minitab'>
+                <div className = "minitabbtn" onClick={()=>updateTabs(1)}>Pomodoro</div>
+                <div  className = "minitabbtn" onClick={()=>updateTabs(2)}>Short Break</div>
+                <div  className = "minitabbtn" onClick={()=>updateTabs(3)}>Long Break</div>
+            </div>
+            <div className='minicontent'>
+                <div className={tabs === 1? "showPomodoro" : "content"}>
+                    <Timer />
+                </div>
+                <div className={tabs === 2? "showShortBreak" : "content"}>
+                    <Timer />
+                </div>
+                <div className={tabs === 3? "showLongBreak" : "content"}>
+                    <Timer />
+                </div>
+            </div>
+            
+            
+           
 
         </div>
     );
