8d2e445aa99c0966c4256803e677238b106ba144
Chris <comosjay@gmail.com>
2024-01-26 01:16:48 -0800

    sql

--- PomodoroDatabase.session.sql
+++ PomodoroDatabase.session.sql
@@ -1,7 +1,10 @@
 -- @block
-CREATE TABLE Users(
+CREATE TABLE users(
     id INT PRIMARY KEY AUTO_INCREMENT, 
     email VARCHAR(255) NOT NULL UNIQUE
     
     
 );
+
+-- @block
+INSERT INTO users(id, email) VALUES (1, 'ar@gmail.com');
--- PomodoroProject/Backend/index.js
+++ PomodoroProject/Backend/index.js
@@ -1,10 +1,35 @@
 
 const express = require('express')
+const mysql = require('mysql')
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("hello world");
+const db = mysql.createPool({
+    host: "localhost",
+    user: 'root',
+    password: 'qwer',
+    database: 'PomodoroDatabase',
 });
+app.get('/', (req, res) => {
+    const sqlInsert = "INSERT INTO users(id, email) VALUES (2, 'br@gmail.com');"
+    
+    
+    db.query(sqlInsert, (err, result) => {
+        res.send("hello worldaa");
+    })
+    
+});
+// db.connect((err) => {
+//     if (err) return console.error(err.message);
+//     let sqlInsert = "INSERT INTO users(id, email) VALUES (1, 'ar@gmail.com');"
+    
+  
+//     // execute the insert statment
+//     connection.query(sqlInsert);
+  
+//     // close the database connection
+//     connection.end();
+//   });
+
 
 app.listen(5174, () =>{
     console.log("yay");
